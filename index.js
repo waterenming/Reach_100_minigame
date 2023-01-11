50c3f924a985d554fc9509e9660a0ff047840166
Ming Shum <wateren0815@gmail.com>
2023-01-11 11:15:15 +0100

    write rolldice function

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-ctx.strokeStyle = "red";
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, Math.PI * 2);
-ctx.stroke();
+let currentscore1 = 0;
+let currentscore2 = 0;
+let player1score = 0;
+let player2score = 0;
+let randomNumber = 0;
+
+const gamestart = document.getElementById("gamestart");
+const roll = document.getElementById("roll");
+const hold = document.getElementById("hold");
+
+gamestart.addEventListener("click", start);
+
+function start() {
+
+}
+
+
+const rolldice = function() {
+    randomNumber = Math.floor(Math.random() * 6) + 1;
+
+//const diceface = document.getElementById("diceimage");
+
+if (randomNumber === 1) {
+    document.getElementById("diceimage").src="images/dice_one.png"; 
+  }
+  else if (randomNumber === 2) {
+    document.getElementById("diceimage").src="images/dice_two.png";
+  }
+  else if (randomNumber === 3) {
+    document.getElementById("diceimage").src="images/dice_three.png";
+  }
+  else if (randomNumber === 4) {
+    document.getElementById("diceimage").src="images/dice_four.png";
+  }
+  else if (randomNumber === 5) {
+    document.getElementById("diceimage").src="images/dice_five.png";
+  }
+  else if (randomNumber === 6) {
+    document.getElementById("diceimage").src="images/dice_six.png";
+  }
+  
+}
+
+roll.addEventListener("click", rolldice);
+
+
+
+
